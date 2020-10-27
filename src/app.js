fa5494b7c34fdeea448ea6bff9a1267dfbf8d88a
Labenea <edokaditya@gmail.com>
2020-10-28 00:43:44 +0800

    adding animation and header

--- src/app.js
+++ src/app.js
@@ -1,2 +1,3 @@
 import "./scripts/component/nav-bar.js";
 import "./assets/scss/style.scss";
+import "./scripts/home.js";
---
+++ src/scripts/home.js
@@ -0,0 +1,8 @@
+setInterval(
+  (appear = () => {
+    var Jtext = document.querySelector(".intro");
+    Jtext.classList.add("appear");
+    console.log("tes");
+  }),
+  500
+);
