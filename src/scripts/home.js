bd818659b1d4b9dad4ad10f52e5fa7d63a5823f1
Labenea <edokaditya@gmail.com>
2020-10-28 02:15:26 +0800

    adding fade on scrol

--- src/scripts/home.js
+++ src/scripts/home.js
@@ -1,8 +1,25 @@
-setInterval(
-  (appear = () => {
-    var Jtext = document.querySelector(".intro");
-    Jtext.classList.add("appear");
-    console.log("tes");
-  }),
-  500
-);
+var start = () => {
+  setInterval(
+    (appear = () => {
+      var Jtext = document.querySelector(".intro");
+      Jtext.classList.add("appear");
+    }),
+    500
+  );
+};
+
+function fadeOnScroll() {
+  var Jtext = document.querySelector(".intro");
+  var Jpos = Jtext.getBoundingClientRect().top;
+  window.addEventListener("scroll", () => {
+    var curScroll = window.pageYOffset;
+    if (curScroll <= Jpos) {
+      opacity = 1 - (curScroll / Jpos) * 1.2;
+      console.log(opacity);
+    }
+    Jtext.style.opacity = opacity;
+  });
+}
+
+start();
+window.addEventListener("scroll", fadeOnScroll());
